ea85fbf0489619ae9a8b66eef4bac3fb6420ae80
kedöshim <abraaodpaula@gmail.com>
2022-09-28 17:55:11 -0300

    Binario +  getReview 100%

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,29 @@
 #include <fstream>
 #include <math.h>
 
-void Loading(double i, double n)
+void leArquivoBinario()
+{
+    std::fstream arq("ratings_Electronics.bin", std::ios::in | std::ios::binary);
+
+        if(arq.is_open())
+        {
+            char str[10];
+
+            arq.read(str, 5);
+            str[5] = '\0';
+        }
+        else
+            std::cerr << "ERRO: O arquivo nao pode ser aberto!" << std::endl;
+}
+
+
+void loading(double i, double n)
 {
     int ratio = trunc(10*i/n);
 
     switch (ratio)
     {
+    case 0 :std::cout<<"[          ] 0%\n";break;
     case 1 :std::cout<<"[&         ] 10%\n";break;
     case 2 :std::cout<<"[&&        ] 20%\n";break;
     case 3 :std::cout<<"[&&&       ] 30%\n";break;
@@ -24,7 +41,37 @@ void Loading(double i, double n)
     return;
 }
 
-std::vector<ProductReview*> loadReviews(std::string path, int nReviews){
+void getReview(int i)
+{
+    int size = 15+11+3+11;
+    std::ifstream bin("ratings_Electronics.bin",std::ios::in|std::ios::binary);
+    if(bin.is_open())
+    {
+        char str[4][16];
+        int s[4]={15,11,3,11}; //são o numero de bits de cada uma das variaveis
+                                //daria pra fazer isso como var globais
+        bin.seekg(0, bin.beg);
+        bin.ignore((size*i),EOF);
+        //std::cout<<"posicao:"<<bin.tellg()<<"\n";
+        
+        for(int j=0;j<4;j++)
+        {
+            bin.read(str[j],s[j]);
+        }
+        
+        std::string user(str[0]);
+        std::string product(str[1]);
+        std::string rate(str[2]);
+        std::string time(str[3]);
+
+        std::cout<<"-------------\nReview "<<i<<":\n";
+
+        std::cout<<"\n UserId: "<<user<<"\n ProductId: "<<product<<"\n Rating: "<<str[2]<<"\n Timestamp: "<<time<<"\n-------------";
+    }
+    
+}   
+
+std::vector<ProductReview*> loadReviews(std::string path, double nReviews){
  
     std::ifstream loader(path);
     std::string line;
@@ -41,7 +88,7 @@ std::vector<ProductReview*> loadReviews(std::string path, int nReviews){
             reviews.push_back(a);
             //reviews.back()->print();
             counter++;
-            Loading(counter,7900000);
+            loading(counter,7900000);
         }
     else
         for(double i=0;i<nReviews&&loader.good();i++)
@@ -50,32 +97,44 @@ std::vector<ProductReview*> loadReviews(std::string path, int nReviews){
             a= new ProductReview(line);
             reviews.push_back(a);
             //reviews.back()->print();
-            Loading(i,nReviews);
+            loading(i,nReviews);
         }
     
     return reviews;
 }
 
-void createBinary(std::string path, int n)
+void createBinary(std::string path, double n)
 {
     std::vector<ProductReview*> reviews;
     reviews = loadReviews(path,n);
     std::string line;
+
+    std::string user,product,rate,time;
+
     std::ofstream eraser("ratings_Electronics.bin"); eraser.close(); //apaga o conteudo do arquivo
     std::ofstream binaryfile("ratings_Electronics.bin",std::ios::app|std::ios::binary);
-    std::cout<<"\nn de reviews:"<<reviews.size()<<"\n";
+    //std::cout<<"\nn de reviews:"<<reviews.size()<<"\n";
     if(n<0)
     {
         for(double i=0;i<reviews.size();i++)
         {
-            binaryfile.write((char*)&reviews[i],sizeof(ProductReview));
+            binaryfile.write((char*)reviews[i],sizeof(ProductReview));
         }
     }
     else
     {
         for(int i=0;i<n;i++)
         {
-            binaryfile.write((char*)&reviews[i],sizeof(ProductReview));
+            user=reviews[i]->getUserId();
+            product=reviews[i]->getProductId();
+            rate=reviews[i]->getRating();
+            time=reviews[i]->getTime();
+
+            binaryfile.write(reinterpret_cast<const char*>(user.c_str()),15);  //15 é o numero de caracteres maximo +1
+            binaryfile.write(reinterpret_cast<const char*>(product.c_str()),11);
+            binaryfile.write(reinterpret_cast<const char*>(rate.c_str()),3);
+            binaryfile.write(reinterpret_cast<const char*>(time.c_str()),11);
+            
         }
     }
         
@@ -83,7 +142,7 @@ void createBinary(std::string path, int n)
 
 }
 
-void goToBinaryReview(std::ifstream& file, unsigned int num)
+/*void goToBinaryReview(std::ifstream& file, unsigned int num)
 {
     double size = sizeof(ProductReview);
     file.seekg(std::ios::beg);
@@ -98,21 +157,34 @@ void getReview(int i)
     std::ofstream printer("../ratings_Electronics.bin");
     goToBinaryReview(binaryRead,i);
     
-}
+}*/
 
 int main(int argc, char **argv)
 {
-    int n=0;
+    double n=0;
+    double i=0;
     std::string path(argv[1]);
 
     std::cout << "Caminho do arquivo especificado:"<<path<<"\n";
 
     std::cout<<"\n"<<"Quantas reviews devem ser analizadas?"<<"\n";
     std::cin>>n;
+    if(n<1)
+        return 0;
     
-    //reviews=loadReviews(path,n);
     createBinary(path,n);
-    //void getReview(int i);
+
+    std::cout<<"\n"<<"Binario Carregado!"<<"\n";
+
+        std::cout<<"\n"<<"Insira o indice da review desejada:"<<"\n";
+        std::cin>>i;
+    while(i>=0)
+    {
+        if(i>=n){std::cout<<"Nao existem reviews com esse indice\n";std::cin>>i;continue;}
+        getReview(i);
+        std::cout<<"\n"<<"Insira o indice da review desejada:"<<"\n";
+        std::cin>>i;
+    }
     //ProductReview* import(int n);
     return 0;
 }
