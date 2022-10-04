815eeb26eb2e2a2d2e47b947c46b92882090c7f3
kedöshim <abraaodpaula@gmail.com>
2022-10-04 13:17:18 -0300

    Added string to value function

--- include/hash.h
+++ include/hash.h
@@ -1,3 +1,15 @@
-//#include "sort.h"
+#include "sort.h"
 
 ProductReview* createTable(int n);
+
+double valueString(std::string s)
+{
+    const char* c = s.c_str();
+    float p = 2;
+    double value=0;
+    for(int i=0;i<s.length();i++)
+    {
+        value+=(((int)c[i])-48)*pow(p,i);
+    }
+    return value;
+}
