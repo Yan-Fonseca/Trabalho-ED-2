ef3cdb16bf9da0591dfe8f27f6556ec12e26ee7d
Yan Fonseca <yanfonseca2002@gmail.com>
2022-10-16 21:28:22 -0300

    Adição da configuração do diretório de testes e exemplificação com o arquivo radixsort.cc. CTest configurado e funcional

---
+++ tests/radixsort.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int one = 1;
+    if(one==1){
+        return 0;
+    }
+    else{
+        return 1;
+    }
+}
