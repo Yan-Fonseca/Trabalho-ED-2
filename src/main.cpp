9a42c0ae3a7fc1612a3f9a56c2f9196582c832ea
Yan Fonseca <yanfonseca2002@gmail.com>
2022-12-02 21:56:32 -0300

    Alterando a função de criar o binário na main

--- src/main.cpp
+++ src/main.cpp
@@ -49,7 +49,7 @@ int main(int argc, char **argv)
     double timeForCreateBinary;
     std::chrono::high_resolution_clock::time_point inicio = std::chrono::high_resolution_clock::now();
 
-    createBinary(path,nReviews);
+    createBinary2(path,nReviews);
     nReviews = getSize();
 
     std::chrono::high_resolution_clock::time_point fim = std::chrono::high_resolution_clock::now();
